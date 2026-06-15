82e6f8c4bc0a7a32c704e772261eb37e5ed79c18
eluney8 <eluneyibarstsdsm@gmail.com>
2026-06-15 20:45:46 -0300

    feat: se realizó la logica para la funcionalidad de obtener un personaje por ID.

--- src/controllers/personajes.controllers.js
+++ src/controllers/personajes.controllers.js
@@ -4,3 +4,20 @@ export const obtenerTodosLosPersonajes = (req, res) => {
     res.json(personajes);
 };
 
+export const obtenerUnPersonajePorId = (req, res) => {
+    const idPersonaje = req.params.id; 
+    const idNumerico = Number(idPersonaje); 
+
+    if (isNaN(idNumerico)) {
+        return res.status(400).json({ message: "El ID debe ser un numero valido" });
+    }
+    const personajeEncontrado = personajes.find((p) => p.id === idNumerico);
+
+    if (!personajeEncontrado) {
+        return res.status(404).json({ 
+            message: `Personaje con el id #${idPersonaje} no encontrado` 
+        });
+    }
+
+    res.json(personajeEncontrado);
+};
--- src/routes/personajes.routes.js
+++ src/routes/personajes.routes.js
@@ -1,6 +1,9 @@
 import { Router } from "express";
-import { obtenerTodosLosPersonajes } from "../controllers/personajes.controllers.js";
+import { obtenerTodosLosPersonajes,
+         obtenerUnPersonajePorId 
+ } from "../controllers/personajes.controllers.js";
 
 export const personajeRouter = Router();
 
 personajeRouter.get("/characters", obtenerTodosLosPersonajes);
+personajeRouter.get("/characters/:id", obtenerUnPersonajePorId);
