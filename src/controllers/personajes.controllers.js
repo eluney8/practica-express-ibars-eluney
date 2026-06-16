3a27b471adbc41e1629c19cc6a3854baea97b001
eluney8 <eluneyibarstsdsm@gmail.com>
2026-06-15 21:23:14 -0300

    feat: se realizó la logica para la funcionalidad de crear un nuevo personaje.

--- src/controllers/personajes.controllers.js
+++ src/controllers/personajes.controllers.js
@@ -1,23 +1,46 @@
 import { personajes } from "../data/personajes.js";
 
 export const obtenerTodosLosPersonajes = (req, res) => {
-    res.json(personajes);
+  res.json(personajes);
 };
 
 export const obtenerUnPersonajePorId = (req, res) => {
-    const idPersonaje = req.params.id; 
-    const idNumerico = Number(idPersonaje); 
+  const idPersonaje = req.params.id;
+  const idNumerico = Number(idPersonaje);
 
-    if (isNaN(idNumerico)) {
-        return res.status(400).json({ message: "El ID debe ser un numero valido" });
-    }
-    const personajeEncontrado = personajes.find((p) => p.id === idNumerico);
+  if (isNaN(idNumerico)) {
+    return res.status(400).json({ message: "El ID debe ser un numero valido" });
+  }
+  const personajeEncontrado = personajes.find((p) => p.id === idNumerico);
 
-    if (!personajeEncontrado) {
-        return res.status(404).json({ 
-            message: `Personaje con el id #${idPersonaje} no encontrado` 
-        });
-    }
+  if (!personajeEncontrado) {
+    return res.status(404).json({
+      message: `Personaje con el id #${idPersonaje} no encontrado`,
+    });
+  }
 
-    res.json(personajeEncontrado);
+  res.json(personajeEncontrado);
+};
+
+export const crearUnPersonaje = (req, res) => {
+  const { nombre, imagen } = req.body;
+
+  if (!nombre || nombre === "" || !imagen || imagen === "") {
+    return res.status(400).json({
+      message:
+        "Los campos nombre e imagen son obligatorios y no pueden estar vacios",
+    });
+  }
+  const nuevoPersonaje = {
+    id: Date.now(),
+    nombre,
+    imagen,
+  };
+
+  personajes.push(nuevoPersonaje);
+
+  res.status(201).json({
+    message: "Personaje creado correctamente",
+    nuevoPersonaje,
+  });
 };
--- src/routes/personajes.routes.js
+++ src/routes/personajes.routes.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import { obtenerTodosLosPersonajes,
-         obtenerUnPersonajePorId 
+         obtenerUnPersonajePorId,
+         crearUnPersonaje
  } from "../controllers/personajes.controllers.js";
 
 export const personajeRouter = Router();
 
 personajeRouter.get("/characters", obtenerTodosLosPersonajes);
 personajeRouter.get("/characters/:id", obtenerUnPersonajePorId);
+personajeRouter.post("/characters", crearUnPersonaje);
